15889148e5fa53f821ced168e7b99a9d56e44334
Andy Xiang <andy@ttyn.me>
2016-05-03 15:20:49 -0700

    the week5 class work

---
+++ Timeable.h
@@ -0,0 +1,1 @@
+../Timeable.h
---
+++ main.cpp
@@ -0,0 +1,33 @@
+/*
+   1. insert of random elements into fhbst
+   2. sequential insert = O(n^2)
+   3. shuffled insert: T(n) = an + bn + c log(n) = O(n) linear
+   4. Lookup time
+ 
+   BSTLookupExpts
+      * SeqInsertionExpt
+      * ShuffledInsertonExpt.
+
+      functor would just do a random numm generator and search
+
+   5. 25 data points - and get the time to run.
+ 
+ 
+*/
+
+
+#include <iosteam>
+#include "FHBST.h"
+#include <ctime>
+
+#include "Timeable.h"
+
+using namespace std;
+
+
+int main()
+{
+
+   FHBST bst;
+   
+}
