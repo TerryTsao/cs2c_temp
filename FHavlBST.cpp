1deb8f4ae374748a047707c270456d1e1798c3f3
Andy Xiang <andy@ttyn.me>
2016-05-10 14:28:16 -0700

    correct the name to avl

--- FHavlBST.cpp
+++ FHavlBST.cpp
@@ -13,58 +13,58 @@ using std::vector;
 using std::cout;
 using std::endl;
 template <class Comparable>
-class FHvalNode : public FHNode<Comparable>
+class FHavlNode : public FHNode<Comparable>
 {
 private:
    int height;
    
 public:
-   FHvalNode(const Comparable &d = Comparable(),
-             FHvalNode<Comparable> *ltf = NULL,
-             FHvalNode<Comparable> *rt = NULL,
+   FHavlNode(const Comparable &d = Comparable(),
+             FHavlNode<Comparable> *ltf = NULL,
+             FHavlNode<Comparable> *rt = NULL,
              int height =0 )
-   : FHvalNode<Comparable>(d, ltf, rt), height(height) {}
+   : FHavlNode<Comparable>(d, ltf, rt), height(height) {}
    int getHeight() {return height;}
    bool setHeight(int h) { if(h <1) return false; this->height =h; return true;}
 };
 
 template <class Comparable>
-class FHvalBST : public FHBST<Comparable>
+class FHavlBST : public FHBST<Comparable>
 {
 protected:
-   void rotateWithLeft(FHvalNode<Comparable> * &k2);
-   void rotateWithRight(FHvalNode<Comparable> * &k2);
-   void doubleRotateWithLeft(FHvalNode<Comparable> * &k2);
-   void doubleRotateWithRight(FHvalNode<Comparable> * &k2);
-   static int heightOf(const FHvalNode<Comparable> *p) { return p == NULL ? -1 : p->getHeight(); }
+   void rotateWithLeft(FHavlNode<Comparable> * &k2);
+   void rotateWithRight(FHavlNode<Comparable> * &k2);
+   void doubleRotateWithLeft(FHavlNode<Comparable> * &k2);
+   void doubleRotateWithRight(FHavlNode<Comparable> * &k2);
+   static int heightOf(const FHavlNode<Comparable> *p) { return p == NULL ? -1 : p->getHeight(); }
    
-   bool insert (const Comparable &x, FHvalNode<Comparable> * &node);
-   bool remove (const Comparable &x, FHvalNode<Comparable> * &node);
+   bool insert (const Comparable &x, FHavlNode<Comparable> * &node);
+   bool remove (const Comparable &x, FHavlNode<Comparable> * &node);
 public:
    bool insert(const Comparable &x) { return insert(x, this->mRoot); }
    bool remove(const Comparable &x) { return remove(x, this->mRoot); }
 };
 
 template <class Comparable>
-void FHvalBST<Comparable> :: rotateWithLeft(FHvalNode<Comparable> * &k2)
+void FHavlBST<Comparable> :: rotateWithLeft(FHavlNode<Comparable> * &k2)
 {
-   FHvalNode<Comparable> *k1 = k2->left;
+   FHavlNode<Comparable> *k1 = k2->left;
    k2->left = k1->right;
    k1->right = k2;
    k2 = k1;
 }
 
 template <class Comparable>
-void FHvalBST<Comparable> :: rotateWithRight(FHvalNode<Comparable> * &k2)
+void FHavlBST<Comparable> :: rotateWithRight(FHavlNode<Comparable> * &k2)
 {
-   FHvalNode<Comparable> *k1 = k2->right;
+   FHavlNode<Comparable> *k1 = k2->right;
    k2->right = k1->left;
    k1->left = k2;
    k2 = k1;
 }
 
 template <class Comparable>
-void FHvalBST<Comparable> :: doubleRotateWithLeft(FHvalNode<Comparable> * &k2)
+void FHavlBST<Comparable> :: doubleRotateWithLeft(FHavlNode<Comparable> * &k2)
 {
    rotateWithRight(k2->left);
    rotateWithLeft(k2);
@@ -84,12 +84,12 @@ void FHvalBST<Comparable> :: doubleRotateWithLeft(FHvalNode<Comparable> * &k2)
 int main()
 {
    const int TREESIZE = 10; // change to after test 1000000
-   FHvalBST<int> aValTree;
+   FHavlBST<int> anAvlTree;
    
    // fill a tree
    for (int i=0; i<TREESIZE; i++)
-      aValTree.insert( arc4random() % 200);
+      anAvlTree.insert( arc4random() % 200);
    
-   aValTree.to_string();
+   anAvlTree.to_string();
    
 }
